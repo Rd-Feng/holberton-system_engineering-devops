018140211e138dc318c78e171cf5d97f92f2eb8d
Rd-Feng <394@holbertonschool.com>
2019-02-13 12:49:23 -0800

    return None on error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,11 +18,15 @@ def recurse(subreddit, after=None, hot_list=[]):
                      allow_redirects=False
                      )
     if r.status_code != 200:
-        return hot_list
+        return None
     else:
         posts = r.json().get('data').get('children')
         for post in posts:
             hot_list.append(post.get('data').get('title'))
         if r.json().get('data').get('after'):
-            return recurse(subreddit, after=r.json().get('data').get('after'), hot_list=hot_list)
+            return recurse(
+                subreddit,
+                after=r.json().get('data').get('after'),
+                hot_list=hot_list
+            )
         return hot_list
